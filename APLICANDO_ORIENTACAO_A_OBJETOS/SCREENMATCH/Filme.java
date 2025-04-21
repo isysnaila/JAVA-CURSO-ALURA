8ade6ed18ac5cfebd2bff6d16ec260ee528260a5
Isys Naila <nailaisys2017@gmail.com>
2025-04-21 18:04:25 -0300

    update
    incluindo os métodos get e set

--- APLICANDO_ORIENTACAO_A_OBJETOS/SCREENMATCH/Filme.java
+++ APLICANDO_ORIENTACAO_A_OBJETOS/SCREENMATCH/Filme.java
@@ -1,23 +1,59 @@
 public class Filme {
-    String nome;
-    int anoLancamento;
-    boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalAvaliacoes;
-    int duracaoEmMinutos;
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
 
-    void exibeFichaTecnica(){
+    public boolean getIncluidoNoPlano(){
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getAnoDeLancamento(){
+        return anoLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoLancamento){
+        this.anoLancamento = anoLancamento;
+    }
+
+    public int getDuracaoEmMinutos(){
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes(){
+        return totalAvaliacoes;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void exibeFichaTecnica(){
         System.out.println("Nome de Filme: " + nome);
         System.out.println("Ano de Lançamento: "+ anoLancamento);
         System.out.println("Duração: " + duracaoEmMinutos + " minutos.");
     }
 
-    void avalia(double nota){
+    public void avalia(double nota){
         somaDasAvaliacoes += nota;
         totalAvaliacoes++;
     }
 
-    double pegaMedia(){
+    public double pegaMedia(){
         return somaDasAvaliacoes/totalAvaliacoes;
     }
 }
--- APLICANDO_ORIENTACAO_A_OBJETOS/SCREENMATCH/Main.java
+++ APLICANDO_ORIENTACAO_A_OBJETOS/SCREENMATCH/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Filme mammaMia = new Filme(); //tipo de referência
-        mammaMia.nome = "Mamma Mia";
-        mammaMia.anoLancamento = 2008;
-        mammaMia.duracaoEmMinutos = 108;
+        mammaMia.setNome("Mamma Mia");
+        mammaMia.setAnoDeLancamento(2008);
+        mammaMia.setDuracaoEmMinutos(108);
 
         mammaMia.exibeFichaTecnica();
         mammaMia.avalia(8);
         mammaMia.avalia(10);
         mammaMia.avalia(9.5);
-        System.out.println("A soma das avaliações é: "+ mammaMia.somaDasAvaliacoes);
-        System.out.println("O total de avaliações feitas foi: " + mammaMia.totalAvaliacoes);
+        System.out.println("O total de avaliações feitas foi: " + mammaMia.getTotalDeAvaliacoes());
         System.out.println(mammaMia.pegaMedia());
 
     }
